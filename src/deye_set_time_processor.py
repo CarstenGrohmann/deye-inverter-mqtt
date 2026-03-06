f7bcf54978ae89738cb0351a38581c036b124c03
kalev <ygi@pildistaja.com>
2026-03-06 00:19:14 +0000

    formatting improvements

--- src/deye_set_time_processor.py
+++ src/deye_set_time_processor.py
@@ -30,7 +30,8 @@ class DeyeSetTimeProcessor(DeyeEventProcessor):
     Set logger time when the logger becomes available online.
     """
 
-    def __init__(self, logger_config: DeyeLoggerConfig, interval: int, timezone: str, sensors: [Sensor], modbus: DeyeModbus):
+    def __init__(self, logger_config: DeyeLoggerConfig, interval: int, timezone: str,
+        sensors: [Sensor], modbus: DeyeModbus):
         self.__log = logger_config.logger_adapter(logging.getLogger(DeyeSetTimeProcessor.__name__))
         self.__interval = interval
         self.__modbus = modbus
